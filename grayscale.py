02b797d150116173a15da67c0518bf51ace5ab0a
SidhaantThakker <sidhaantthakker@gmail.com>
2021-09-25 18:04:28 +0530

    add greyscale feed file

---
+++ grayscale.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read(0)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Video Feed', frame)
+    cv2.imshow('Greyscale Feed', gray)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
